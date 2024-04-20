b7d937ec69bad584680afb2c2509812262e5b15d
Sebastien Bürky <me@novadragon.de>
2024-04-20 23:19:35 +0200

    Prefer to use Iconset Icon where i could find a code

--- installer.sh
+++ installer.sh
@@ -81,6 +81,9 @@ function waFindInstalled() {
 }
 
 function waConfigureApp() {
+	if [ -z "${ICON}" ]; then
+		ICON=${SYS_PATH}/apps/${1}/icon.${2}
+	fi
 	. "${SYS_PATH}/apps/${1}/info"
 	echo -n "  Configuring ${NAME}..."
 	if [ ${USEDEMO} != 1 ]; then
@@ -90,7 +93,7 @@ Name=${NAME}
 Exec=${BIN_PATH}/winapps ${1} %F
 Terminal=false
 Type=Application
-Icon=${SYS_PATH}/apps/${1}/icon.${2}
+Icon=$ICON
 StartupWMClass=${FULL_NAME}
 Comment=${FULL_NAME}
 Categories=${CATEGORIES}
@@ -103,6 +106,7 @@ ${BIN_PATH}/winapps ${1} $@
 		${SUDO} chmod a+x "${BIN_PATH}/${1}"
 	fi
 	echo " Finished."
+	ICON=""
 }
 
 function waConfigureApps() {
@@ -246,7 +250,7 @@ Name=Windows
 Exec=${BIN_PATH}/winapps windows %F
 Terminal=false
 Type=Application
-Icon=${SYS_PATH}/icons/windows.svg
+Icon=distributor-logo-windows
 StartupWMClass=Microsoft Windows
 Comment=Microsoft Windows
 Categories=Windows
